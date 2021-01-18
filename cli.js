651224aa9e30df6093ee791fb3b88fd3b7455a5a
alestor123 <alestoraldous@gmail.com>
2021-01-18 15:36:40 +0530

    added cli code

--- cli.js
+++ cli.js
@@ -1,1 +1,11 @@
 #!/usr/bin/env node
+var httpcat = require('./App'),
+fs = require('fs');
+
+(async () => {
+var statscode = process.argv[2],
+data = await httpcat(statscode)
+fs.writeFileSync(`${statscode}.jpg`, data.buffer, (err) => {
+if (err) throw err;
+})})();
+
