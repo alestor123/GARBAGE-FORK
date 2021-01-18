628184180aea808258f8cfc507db5e4832e55df6
alestor123 <alestoraldous@gmail.com>
2021-01-18 15:30:54 +0530

    added main code

--- App.js
+++ App.js
@@ -1,4 +1,9 @@
 var axios = require('axios')
 module.exports = async code => {
 if(!code) throw Error('Code Not Found')
+var data = await axios.get(`https://http.cat/${code}`,  {responseType: 'arraybuffer'})
+return {
+    buffer:data.data,
+    url:`https://http.cat/${code}`
+}
 }
